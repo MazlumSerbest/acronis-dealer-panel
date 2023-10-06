b51136630dca014be501e86d1dc8e03b8f36f1da
MazlumSerbest <mazlumserbest93@gmail.com>
2023-10-06 19:26:35 +0300

    Added getToken method

---
+++ src/lib/getToken.ts
@@ -0,0 +1,21 @@
+import "server-only";
+export default async function getToken() {
+    const encodedClientCreds = btoa(
+        `${process.env.ACRONIS_CLIENT_ID}:${process.env.ACRONIS_CLIENT_SECRET}`,
+    );
+    const headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${encodedClientCreds}`,
+    };
+    const res = await fetch(`${process.env.ACRONIS_API_V2_URL}/idp/token`, {
+        method: "POST",
+        headers: headers,
+        body: new URLSearchParams({
+            grant_type: "client_credentials",
+        }),
+        next: { revalidate: 5400 },
+    });
+    
+    const auth = await res.json();
+    return await auth.access_token;
+}
