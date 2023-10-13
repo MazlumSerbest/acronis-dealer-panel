b2b971c5995dee19b1e30dc01a44c3554aeb7c03
MazlumSerbest <mazlumserbest93@gmail.com>
2023-10-13 20:48:47 +0300

    Added panel entry oage

--- src/app/[locale]/panel/page.tsx
+++ src/app/[locale]/panel/page.tsx
@@ -1,5 +1,87 @@
-import { Card } from "@nextui-org/react";
+import { Card, CardBody } from "@nextui-org/card";
+import { CircularProgress, Progress } from "@nextui-org/progress";
+import { BiDesktop, BiShield, BiData } from "react-icons/bi";
+import PanelCard from "@/components/PanelCard";
 
 export default function PanelPage() {
-    return <>Home</>;
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 mt-4 md:mt-0">
+            <PanelCard
+                header="Total Machines"
+                content={
+                    <h1 className="font-bold text-7xl text-zinc-500">20</h1>
+                }
+                icon={
+                    <BiDesktop className="text-5xl md:text-6xl text-blue-400/50" />
+                }
+            />
+            <PanelCard
+                header="Total License"
+                content={
+                    <h1 className="font-bold text-7xl text-zinc-500">150</h1>
+                }
+                icon={
+                    <BiShield className="text-5xl md:text-6xl text-blue-400/50" />
+                }
+            />
+            <PanelCard
+                header="Total Location"
+                content={
+                    <h1 className="font-bold text-7xl text-zinc-500">58</h1>
+                }
+                icon={
+                    <BiData className="text-5xl md:text-6xl text-blue-400/50" />
+                }
+            />
+            <PanelCard
+                header="Total Usage"
+                content={
+                    <CircularProgress
+                        value={120}
+                        maxValue={500}
+                        showValueLabel={true}
+                        size="lg"
+                        strokeWidth={10}
+                        formatOptions={{
+                            style: "unit",
+                            unit: "gigabyte",
+                        }}
+                        classNames={{
+                            svg: "w-32 h-32 drop-shadow-md",
+                            indicator: "text-green-400",
+                            value: "text-2xl font-semibold text-green-400",
+                        }}
+                    />
+                }
+            />
+            <Card className="col-span-1 md:col-span-2 border-b-4 border-zinc-300">
+                <CardBody className="gap-2 justify-around">
+                    <Progress
+                        label="Total"
+                        showValueLabel={true}
+                        value={90}
+                        classNames={{
+                            label: "text-zinc-500 font-semibold",
+                            indicator: "bg-blue-400",
+                            value: "text-zinc-500",
+                        }}
+                    />
+                    <Progress
+                        label="Expired Licenses"
+                        showValueLabel={true}
+                        value={500}
+                        maxValue={5000}
+                        formatOptions={{
+                            style: "decimal",
+                        }}
+                        classNames={{
+                            label: "text-zinc-500 font-semibold",
+                            indicator: "bg-red-400",
+                            value: "text-zinc-500",
+                        }}
+                    />
+                </CardBody>
+            </Card>
+        </div>
+    );
 }
