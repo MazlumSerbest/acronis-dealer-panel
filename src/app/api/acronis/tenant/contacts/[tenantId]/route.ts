670523772a3b1a686ebf8a0cc637238b3b6dc79e
MazlumSerbest <mazlumserbest93@gmail.com>
2023-10-18 15:04:55 +0300

    Added contacts API

---
+++ src/app/api/acronis/tenant/contacts/[tenantId]/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+import getToken from "@/lib/getToken";
+
+export async function GET(
+    request: Request,
+    { params }: { params: { tenantId: string } },
+) {
+    try {
+        const tenantId = params.tenantId;
+        const token = await getToken();
+
+        if (token) {
+            console.log(token);
+            const headers = {
+                Authorization: `Bearer ${token}`,
+            };
+            const contactRes = await fetch(
+                `${process.env.ACRONIS_API_V2_URL}/tenants/${tenantId}/contacts`,
+                {
+                    method: "GET",
+                    headers: headers,
+                },
+            );
+
+            if (contactRes.ok) {
+                const contactIds = await contactRes.json();
+                if(!contactIds.items.length) return await NextResponse.json({ users: {} })
+
+                const params = new URLSearchParams({
+                    uuids: contactIds.items.join(),
+                    // lod: "basic",
+                });
+                const res = await fetch(
+                    `${process.env.ACRONIS_API_V2_URL}/contacts?${params}`,
+                    {
+                        method: "GET",
+                        headers: headers,
+                    },
+                );
+
+                if (res.ok) {
+                    const contacts = await res.json();
+                    return await NextResponse.json({ contacts });
+                } else return await NextResponse.json({ message: "Failed!" });
+            } else return await NextResponse.json({ message: "Failed" });
+        } else return NextResponse.json({ message: "Authentication failed!" });
+    } catch (error) {
+        return NextResponse.json({ message: error });
+    }
+}
