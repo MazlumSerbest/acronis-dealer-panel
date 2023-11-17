c09f1e7445afb69585d309d8b38b329307df9ea4
MazlumSerbest <mazlumserbest93@gmail.com>
2023-11-17 16:45:40 +0300

    Zustand store added

---
+++ src/app/store/acronis.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+type State = {
+    userTenant?: Tenant;
+    currentTenant?: Tenant;
+};
+
+type Action = {
+    updateMainTenant: (userTenant: Tenant) => void;
+    updateCurrentTenant: (currentTenant: Tenant) => void;
+};
+
+const useAcronisStore = create<State & Action>((set) => ({
+    // userTenant: {},
+    // currentTenant: {},
+    updateMainTenant: (tenant) => set(() => ({ userTenant: tenant })),
+    updateCurrentTenant: (tenant) => set(() => ({ currentTenant: tenant })),
+}));
+
+export default useAcronisStore;
