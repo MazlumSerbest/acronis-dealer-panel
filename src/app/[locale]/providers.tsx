0317a20d529637ca52c8dc58cc58e86756868a91
MazlumSerbest <mazlumserbest93@gmail.com>
2023-11-17 16:46:01 +0300

    Global swr fetch added

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -1,6 +1,18 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
+import { SWRConfig } from "swr";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return <NextUIProvider>{children}</NextUIProvider>;
+    return (
+        <NextUIProvider>
+            <SWRConfig
+                value={{
+                    // refreshInterval: 3000,
+                    fetcher: (url: string) => fetch(url).then((res) => res.json()),
+                }}
+            >
+                {children}
+            </SWRConfig>
+        </NextUIProvider>
+    );
 }
