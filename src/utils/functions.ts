c1fb6527172261b850cf344d93bece6b6d579c6f
Mazlum Serbest <mazlumserbest93@gmail.com>
2024-06-06 15:37:51 +0300

    Byte calculate function added

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -4,3 +4,12 @@ export function CopyToClipboard(text: string, message?: string) {
     navigator.clipboard.writeText(text);
     toast.success(message ?? "Panoya Kopyalandı!");
 }
+
+export function formatBytes(a: any, b = 2) {
+    if (!+a) return "0 Bytes";
+    const c = 0 > b ? 0 : b,
+        d = Math.floor(Math.log(a) / Math.log(1024));
+    return `${parseFloat((a / Math.pow(1024, d)).toFixed(c))} ${
+        ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"][d]
+    }`;
+}
