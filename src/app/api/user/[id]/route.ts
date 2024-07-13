8198a307f036e08df40470986cfcb45f4612b07b
Mazlum Serbest <mazlumserbest93@gmail.com>
2024-07-13 19:04:03 +0300

    User api routes added

---
+++ src/app/api/user/[id]/route.ts
@@ -0,0 +1,95 @@
+import { NextResponse } from "next/server";
+import prisma from "@/utils/db";
+import { auth } from "@/auth";
+
+export const GET = auth(async (req: any, { params }) => {
+    try {
+        if (!req.auth)
+            return NextResponse.json({
+                message: "Authorization Needed!",
+                status: 401,
+                ok: false,
+            });
+
+        const data = await prisma.user.findUnique({
+            where: {
+                email: params?.id as string,
+            },
+        });
+
+        return NextResponse.json(data);
+    } catch (error) {
+        return NextResponse.json({ message: error, status: 500, ok: false });
+    }
+});
+
+// export const PUT = auth(async (req: any, { params }) => {
+//     try {
+//         if (!req.auth)
+//             return NextResponse.json({
+//                 message: "Authorization Needed!",
+//                 status: 401,
+//                 ok: false,
+//             });
+
+//         const user: User = await req.json();
+//         user.updatedAt = new Date().toISOString();
+//         user.updatedBy = req.auth.user.email;
+
+//         const updatedUser = await prisma.user.update({
+//             data: user,
+//             where: {
+//                 id: params?.id as string,
+//             },
+//         });
+
+//         if (updatedUser.id) {
+//             return NextResponse.json({
+//                 message: "Kullanıcı başarıyla güncellendi!",
+//                 status: 200,
+//                 ok: true,
+//             });
+//         } else {
+//             return NextResponse.json({
+//                 message: "Kullanıcı güncellenemedi!",
+//                 status: 400,
+//                 ok: false,
+//             });
+//         }
+//     } catch (error) {
+//         return NextResponse.json({ message: error, status: 500, ok: false });
+//     }
+// });
+
+export const DELETE = auth(async (req: any, { params }) => {
+    try {
+        if (!req.auth)
+            return NextResponse.json({
+                message: "Authorization Needed!",
+                status: 401,
+                ok: false,
+            });
+
+        const deletedUser = await prisma.user.delete({
+            where: {
+                id: params?.id as string,
+            },
+        });
+
+        if (deletedUser.id) {
+            return NextResponse.json({
+                message: "Kullanıcı başarıyla silindi!",
+                status: 200,
+                ok: true,
+            });
+        } else {
+            return NextResponse.json({
+                message: "Kullanıcı silinemedi!",
+                status: 400,
+                ok: false,
+            });
+        }
+    } catch (error) {
+        return NextResponse.json({ message: error, status: 500, ok: false });
+    }
+});
---
+++ src/app/api/user/route.ts
@@ -0,0 +1,82 @@
+import { NextResponse } from "next/server";
+import { auth } from "@/auth";
+import prisma from "@/utils/db";
+
+export const GET = auth(async (req: any) => {
+    try {
+        if (!req.auth)
+            return NextResponse.json({
+                message: "Authorization Needed!",
+                status: 401,
+                ok: false,
+            });
+
+        const data = await prisma.user.findMany({
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                role: true,
+                // createdBy: true,
+                createdAt: true,
+                // updatedBy: true,
+                updatedAt: true,
+            },
+            orderBy: {
+                createdAt: "asc",
+            },
+        });
+
+        return NextResponse.json(data);
+    } catch (error) {
+        return NextResponse.json({ message: error, status: 500, ok: false });
+    }
+});
+
+export const POST = auth(async (req: any) => {
+    try {
+        if (!req.auth)
+            return NextResponse.json({
+                message: "Authorization Needed!",
+                status: 401,
+                ok: false,
+            });
+
+        const user = await req.json();
+        user.createdBy = req.auth.user.email;
+
+        const checkEmail = await prisma.user.findUnique({
+            where: {
+                email: user.email,
+            },
+            select: {
+                email: true,
+            },
+        });
+        if (checkEmail)
+            return NextResponse.json({
+                message: "Bu e-posta önceden kullanılmıştır!",
+                status: 400,
+                ok: false,
+            });
+
+        const newUser = await prisma.user.create({
+            data: user,
+        });
+        if (newUser.id) {
+            return NextResponse.json({
+                message: "Kullanıcı başarıyla kaydedildi!",
+                status: 200,
+                ok: true,
+            });
+        } else {
+            return NextResponse.json({
+                message: "Kullanıcı kaydedilemedi!",
+                status: 400,
+                ok: false,
+            });
+        }
+    } catch (error) {
+        return NextResponse.json({ message: error, status: 500, ok: false });
+    }
+});
