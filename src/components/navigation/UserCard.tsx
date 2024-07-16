34c6438fc9274228454fe433bb6606f23c727403
Mazlum Serbest <mazlumserbest93@gmail.com>
2024-07-16 21:33:50 +0300

    Profile settings added to UserCard

--- src/components/navigation/UserCard.tsx
+++ src/components/navigation/UserCard.tsx
@@ -1,6 +1,5 @@
-import { useState, useEffect } from "react";
-import useSWR from "swr";
-import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useRouter, usePathname } from "next/navigation";
 import { signOut, useSession } from "next-auth/react";
 
 import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
@@ -17,14 +16,34 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Button } from "../ui/button";
 
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import { LuLogOut } from "react-icons/lu";
 import useUserStore from "@/store/user";
 import useAcronisStore from "@/store/acronis";
 import { getFullNameInitials } from "@/lib/utils";
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "../ui/sheet";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "../ui/select";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 
 export default function UserCard() {
     const t = useTranslations("General");
+    const locale = useLocale();
+    const router = useRouter();
+    const pathName = usePathname();
     const session = useSession();
     const { user, updateUser } = useUserStore();
     const { updateMainTenant } = useAcronisStore();
@@ -40,22 +59,22 @@ export default function UserCard() {
                         role: data?.role,
                         name: data?.name,
                         email: data?.email,
-                        acronisId: data?.acronisId,
+                        acronisTenantId: data?.acronisTenantId,
                         createdAt: data?.createdAt,
                         createdBy: "admin",
                     });
                 });
         }
 
-        if (user?.acronisId)
-            fetch(`/api/acronis/tenants/${user?.acronisId}`)
+        if (user?.acronisTenantId)
+            fetch(`/api/acronis/tenants/${user?.acronisTenantId}`)
                 .then((res) => res.json())
                 .then((data) => updateMainTenant(data?.tenant));
     }, [
         session?.data?.user,
         updateMainTenant,
         updateUser,
-        user?.acronisId,
+        user?.acronisTenantId,
     ]);
 
     // if (error) return <div>failed to load</div>;
@@ -72,18 +91,82 @@ export default function UserCard() {
         );
     return (
         <div className="flex gap-2 items-center">
-            <Avatar>
-                <AvatarImage />
-                <AvatarFallback className="bg-blue-100 text-blue-400">
-                    {getFullNameInitials(user?.name || "")}
-                </AvatarFallback>
-            </Avatar>
-            <div className="flex-1 min-w-0">
-                <p className="truncate text-sm font-bold text-blue-400">
-                    {user?.name}
-                </p>
-                <p className="truncate text-xs text-zinc-600">{user?.email}</p>
-            </div>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <div className="group truncate flex gap-2">
+                        <Avatar>
+                            <AvatarImage />
+                            <AvatarFallback className="bg-blue-100 text-blue-400">
+                                {getFullNameInitials(user?.name || "")}
+                            </AvatarFallback>
+                        </Avatar>
+                        <div className="flex-1 min-w-0">
+                            <p className="truncate text-sm font-bold text-blue-400 group-hover:underline group-hover:cursor-pointer">
+                                {user?.name}
+                            </p>
+                            <p className="truncate text-xs text-zinc-600">
+                                {user?.email}
+                            </p>
+                        </div>
+                    </div>
+                </SheetTrigger>
+                <SheetContent>
+                    <SheetHeader>
+                        <SheetTitle className="text-blue-400">{t("editProfile")}</SheetTitle>
+                    </SheetHeader>
+                    <div className="flex flex-col gap-4 my-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="name">{t("name")}</Label>
+                            <Input
+                                readOnly
+                                id="name"
+                                value={user.name}
+                                className="col-span-3"
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <Label htmlFor="username" className="">
+                                {t("email")}
+                            </Label>
+                            <Input
+                                readOnly
+                                id="username"
+                                value={user.email}
+                                className="col-span-3"
+                            />
+                        </div>
+                    </div>
+                    <SheetHeader>
+                        <SheetTitle className="text-blue-400">{t("otherSettings")}</SheetTitle>
+                    </SheetHeader>
+                    <div className="flex flex-col mt-4">
+                        <div className="grid grid-cols-2 items-center gap-4">
+                            <Label>{t("language")}</Label>
+                            <Select
+                                defaultValue={locale}
+                                onValueChange={(v) => {
+                                    const newPath = pathName.replace(
+                                        `/${locale}/`,
+                                        `/${v}/`,
+                                    );
+                                    router.push(newPath);
+                                }}
+                            >
+                                <SelectTrigger>
+                                    <SelectValue
+                                        className="col-span-1"
+                                        placeholder="Language"
+                                    />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="tr">Türkçe</SelectItem>
+                                    <SelectItem value="en">English</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                </SheetContent>
+            </Sheet>
             <AlertDialog>
                 <AlertDialogTrigger asChild>
                     <Button variant="ghost" size="icon">
