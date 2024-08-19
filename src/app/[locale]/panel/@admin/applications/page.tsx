b730c5a518c0dee8938d5d5da05b012eed2dff2a
Mazlum Serbest <mazlumserbest93@gmail.com>
2024-08-19 23:35:38 +0300

    Applications tab fixed

--- src/app/[locale]/panel/@admin/applications/page.tsx
+++ src/app/[locale]/panel/@admin/applications/page.tsx
@@ -10,23 +10,21 @@ export default function ApplicationsPage() {
     const ta = useTranslations("Applications");
 
     return (
-        <div className="flex flex-col w-full items-center">
-            <Tabs defaultValue="waiting" className="flex flex-col w-full">
-                <TabsList className="mx-auto *:md:w-[200px] mb-2">
-                    <TabsTrigger value="waiting">{ta("waiting")}</TabsTrigger>
-                    <TabsTrigger value="approved">{ta("approved")}</TabsTrigger>
-                    <TabsTrigger value="resolved">{ta("resolved")}</TabsTrigger>
-                </TabsList>
-                <TabsContent value="waiting">
-                    <WaitingTab />
-                </TabsContent>
-                <TabsContent value="approved">
-                    <ApprovedTab />
-                </TabsContent>
-                <TabsContent value="resolved">
-                    <ResolvedTab />
-                </TabsContent>
-            </Tabs>
-        </div>
+        <Tabs defaultValue="waiting" className="flex flex-col w-full">
+            <TabsList className="max-w-fit">
+                <TabsTrigger value="waiting">{ta("waiting")}</TabsTrigger>
+                <TabsTrigger value="approved">{ta("approved")}</TabsTrigger>
+                <TabsTrigger value="resolved">{ta("resolved")}</TabsTrigger>
+            </TabsList>
+            <TabsContent value="waiting">
+                <WaitingTab />
+            </TabsContent>
+            <TabsContent value="approved">
+                <ApprovedTab />
+            </TabsContent>
+            <TabsContent value="resolved">
+                <ResolvedTab />
+            </TabsContent>
+        </Tabs>
     );
 }
