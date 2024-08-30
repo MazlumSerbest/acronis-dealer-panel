b0c32b8e76f3a9e22d4e818efc92cf9d622a966c
Mazlum Serbest <mazlumserbest93@gmail.com>
2024-08-30 21:58:39 +0300

    Back to sign in button added to auth error pages

--- src/app/[locale]/(auth)/error/page.tsx
+++ src/app/[locale]/(auth)/error/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 import { auth } from "@/auth";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getTranslations } from "next-intl/server";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Error({
     searchParams,
@@ -27,12 +29,21 @@ export default async function Error({
                         {t("errorTitle")}
                     </CardTitle>
                 </CardHeader>
-                <CardContent>
+                <CardContent className="flex flex-col gap-6">
                     <p className="text-base text-gray-600">
                         {error === "Configuration" && ts("Configuration")}
                         {error === "AccessDenied" && ts("AccessDenied")}
                         {error === "Verification" && ts("Verification")}
                     </p>
+                    {error != "Configuration" && (
+                        <div className="flex justify-center">
+                            <Button variant="outline" size="lg" asChild>
+                                <Link href="/signin">
+                                    {t("backToSignIn")}
+                                </Link>
+                            </Button>
+                        </div>
+                    )}
                 </CardContent>
             </Card>
         </div>
