25ae1bff0caa019118db2d243ebb880a2ab4f79d
Mazlum Serbest <mazlumserbest93@gmail.com>
2024-09-06 19:50:24 +0300

    License tab structure updated

--- src/app/[locale]/panel/@admin/licenses/page.tsx
+++ src/app/[locale]/panel/@admin/licenses/page.tsx
@@ -1,39 +1,102 @@
 "use client";
-import { useTranslations } from "next-intl";
+import { useState } from "react";
 
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectLabel,
+    SelectSeparator,
+    SelectGroup,
+    SelectValue,
+    SelectTrigger,
+} from "@/components/ui/select";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { useTranslations } from "next-intl";
 import UnassignedTab from "./(tabs)/Unassigned";
 import AssignedTab from "./(tabs)/Assigned";
 import ActiveTab from "./(tabs)/Active";
-import FinishedTab from "./(tabs)/Finished";
+import CompletedTab from "./(tabs)/Completed";
 import ExpiredTab from "./(tabs)/Expired";
 
 export default function LicensesPage() {
     const tl = useTranslations("Licenses");
+    const [openTab, setOpenTab] = useState("unassigned");
+    const [openAssignedTab, setOpenAssignedTab] = useState("active");
 
     return (
-        <Tabs defaultValue="unassigned" className="flex flex-col w-full">
-            <TabsList className="max-w-fit">
-                <TabsTrigger value="unassigned">{tl("unassigned")}</TabsTrigger>
-                <TabsTrigger value="assigned">{tl("assigned")}</TabsTrigger>
-                <TabsTrigger value="active">{tl("active")}</TabsTrigger>
-                <TabsTrigger value="finished">{tl("finished")}</TabsTrigger>
-                <TabsTrigger value="expired">{tl("expired")}</TabsTrigger>
-            </TabsList>
+        <Tabs
+            defaultValue="unassigned"
+            className="flex flex-col w-full"
+            value={openTab}
+            onValueChange={(value) => setOpenTab(value)}
+        >
+            <div className="flex flex-row justify-between">
+                <TabsList className="max-w-fit">
+                    <TabsTrigger value="unassigned">
+                        {tl("unassigned")}
+                    </TabsTrigger>
+                    <TabsTrigger value="assigned">{tl("assigned")}</TabsTrigger>
+                    {/* <TabsTrigger value="active">{tl("active")}</TabsTrigger>
+                    <TabsTrigger value="completed">{tl("completed")}</TabsTrigger>
+                    <TabsTrigger value="expired">{tl("expired")}</TabsTrigger> */}
+                </TabsList>
+                {openTab == "assigned" && (
+                    <div>
+                        <Select
+                            defaultValue="active"
+                            onValueChange={(value) => {
+                                setOpenTab("assigned");
+                                setOpenAssignedTab(value);
+                            }}
+                        >
+                            <SelectTrigger className="w-52">
+                                <SelectValue
+                                    className="text-sm"
+                                    placeholder={tl("status")}
+                                />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>{tl("status")}</SelectLabel>
+                                    <SelectSeparator />
+                                </SelectGroup>
+                                <SelectItem value="assigned">
+                                    {tl("inactive")}
+                                </SelectItem>
+                                <SelectItem value="active">
+                                    {tl("active")}
+                                </SelectItem>
+                                <SelectItem value="completed">
+                                    {tl("completed")}
+                                </SelectItem>
+                                <SelectItem value="expired">
+                                    {tl("expired")}
+                                </SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                )}
+            </div>
             <TabsContent value="unassigned">
                 <UnassignedTab />
             </TabsContent>
-            <TabsContent value="assigned">
-                <AssignedTab />
-            </TabsContent>
-            <TabsContent value="active">
-                <ActiveTab />
-            </TabsContent>
-            <TabsContent value="finished">
-                <FinishedTab />
-            </TabsContent>
-            <TabsContent value="expired">
-                <ExpiredTab />
+            <TabsContent value="assigned" className="mt-0">
+                <Tabs value={openAssignedTab}>
+                    <TabsContent value="assigned">
+                        <AssignedTab />
+                    </TabsContent>
+                    <TabsContent value="active">
+                        <ActiveTab />
+                    </TabsContent>
+                    <TabsContent value="completed">
+                        <CompletedTab />
+                    </TabsContent>
+                    <TabsContent value="expired">
+                        <ExpiredTab />
+                    </TabsContent>
+                </Tabs>
             </TabsContent>
         </Tabs>
     );
