fe1d35f68e5094d87dc1af35550992a88b039ba9
Mazlum Serbest <mazlumserbest93@gmail.com>
2024-09-09 17:20:09 +0300

    General fixes

---
+++ src/app/[locale]/panel/@admin/courses/[courseId]/[chapterId]/page.tsx
@@ -0,0 +1,11 @@
+export default function ChapterDetail({
+    params,
+}: {
+    params: { courseId: string, chapterId: string };
+}) {
+    return (
+        <div>
+            <h1>Chapter Detail</h1>
+        </div>
+    );
+}
---
+++ src/app/[locale]/panel/@admin/courses/[courseId]/page.tsx
@@ -0,0 +1,11 @@
+export default function CourseDetail({
+    params,
+}: {
+    params: { courseId: string };
+}) {
+    return (
+        <div>
+            <h1>Course Detail</h1>
+        </div>
+    );
+}
