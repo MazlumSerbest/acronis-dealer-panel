275467298e79d07f142e8a1755efaf4c3b1ffcea
Mazlum Serbest <mazlumserbest93@gmail.com>
2024-11-14 16:07:22 +0300

    Unnecessary pages and components deleted

--- src/app/[locale]/_activate/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Loader from "@/components/loaders/Loader";
-
-export default function ActivateLoading() {
-    return (
-        <div className="h-80">
-            <Loader />
-        </div>
-    );
-}
