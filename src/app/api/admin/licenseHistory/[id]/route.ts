8f4565b67eeee9646d7b50ef72a73099acf74982
Mazlum Serbest <mazlumserbest93@gmail.com>
2024-11-16 17:37:29 +0300

    licenseHistory route added

---
+++ src/app/api/admin/licenseHistory/[id]/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server";
+import { auth } from "@/auth";
+import prisma from "@/utils/db";
+import { getTranslations } from "next-intl/server";
+
+export const GET = auth(async (req: any, { params }) => {
+    try {
+        const tm = await getTranslations({
+            locale: "en",
+            namespace: "Messages",
+        });
+
+        if (req.auth.user.role !== "admin")
+            return NextResponse.json({
+                message: tm("authorizationNeeded"),
+                status: 401,
+                ok: false,
+            });
+
+        const data = await prisma.licenseHistory.findMany({
+            where: {
+                licenseId: params?.id as string,
+            },
+            include: {
+                partner: {
+                    select: {
+                        name: true,
+                    },
+                },
+                previousPartner: {
+                    select: {
+                        name: true,
+                    },
+                },
+                customer: {
+                    select: {
+                        name: true,
+                    },
+                },
+            },
+        });
+
+        return NextResponse.json(data);
+    } catch (error: any) {
+        return NextResponse.json({
+            message: error?.message,
+            status: 500,
+            ok: false,
+        });
+    }
+});
