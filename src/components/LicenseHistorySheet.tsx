a5c92deb048441c952ef2c3ed6d64a0c736e0d06
Mazlum Serbest <mazlumserbest93@gmail.com>
2024-11-22 23:40:18 +0300

    Null description added to LicenseHistorySheet

--- src/components/LicenseHistorySheet.tsx
+++ src/components/LicenseHistorySheet.tsx
@@ -15,7 +15,7 @@ import {
     TooltipTrigger,
 } from "@/components/ui/tooltip";
 
-import { LuArrowDownCircle } from "react-icons/lu";
+import { LuArrowDownCircle, LuHistory } from "react-icons/lu";
 import { DateTimeFormat } from "@/utils/date";
 
 type Props = {
@@ -25,7 +25,7 @@ type Props = {
 
 export function LicenseHistorySheet({ licenseId, trigger }: Props) {
     const tc = useTranslations("Components");
- 
+
     const { data } = useSWR(`/api/admin/licenseHistory/${licenseId}`, null, {
         revalidateOnFocus: false,
     });
@@ -39,36 +39,43 @@ export function LicenseHistorySheet({ licenseId, trigger }: Props) {
                     </SheetTrigger>
                     <SheetContent className="flex flex-col gap-2">
                         <SheetHeader>
-                            <SheetTitle>{tc("licenseHistory")}</SheetTitle>
+                            <SheetTitle className="flex flex-row items-center gap-3">
+                                {tc("licenseHistory")}
+                                <LuHistory className="size-5" />
+                            </SheetTitle>
                         </SheetHeader>
                         <div className="flex flex-col items-center justify-center h-full gap-4 text-sm overflow-y-auto">
-                            {data?.map((item: any, index: number) => (
-                                <div
-                                    key={index}
-                                    className="flex flex-col items-center gap-4"
-                                >
-                                    <div className="font-bold">
-                                        {tc(item.action)}
+                            {data.length ? (
+                                data?.map((item: any, index: number) => (
+                                    <div
+                                        key={index}
+                                        className="flex flex-col items-center gap-4"
+                                    >
+                                        <div className="font-bold">
+                                            {tc(item.action)}
+                                        </div>
+                                        <div>
+                                            {item.action === "activation"
+                                                ? item.customer.name
+                                                : item.partner.name}
+                                        </div>
+                                        <div className="text-muted-foreground">
+                                            {DateTimeFormat(item.createdAt)}
+                                        </div>
+                                        <div className="text-muted-foreground">
+                                            {item.createdBy}
+                                        </div>
+                                        {index !== data.length - 1 && (
+                                            <LuArrowDownCircle
+                                                strokeWidth={1}
+                                                className="size-10 text-blue-500/50"
+                                            />
+                                        )}
                                     </div>
-                                    <div>
-                                        {item.action === "activation"
-                                            ? item.customer.name
-                                            : item.partner.name}
-                                    </div>
-                                    <div className="text-muted-foreground">
-                                        {DateTimeFormat(item.createdAt)}
-                                    </div>
-                                    <div className="text-muted-foreground">
-                                        {item.createdBy}
-                                    </div>
-                                    {index !== data.length - 1 && (
-                                        <LuArrowDownCircle
-                                            strokeWidth={1}
-                                            className="size-10 text-blue-500/50"
-                                        />
-                                    )}
-                                </div>
-                            ))}
+                                ))
+                            ) : (
+                                <div>{tc("noLicenseHistory")}</div>
+                            )}
                         </div>
                     </SheetContent>
                 </Sheet>
