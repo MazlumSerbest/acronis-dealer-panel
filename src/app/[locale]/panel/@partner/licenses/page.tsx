1969ecc6f244a09a1212654282aa22c588044e3f
Mazlum Serbest <mazlumserbest93@gmail.com>
2024-12-30 23:57:42 +0300

    Description added to partner license page

--- src/app/[locale]/panel/@partner/licenses/page.tsx
+++ src/app/[locale]/panel/@partner/licenses/page.tsx
@@ -9,37 +9,49 @@ import CompletedTab from "./(tabs)/Completed";
 import ExpiredTab from "./(tabs)/Expired";
 
 export default function LicensesPage() {
+    const t = useTranslations("General");
+    const tl = useTranslations("Licenses");
     const router = useRouter();
     const searchParams = useSearchParams();
     const tab = searchParams.get("tab") || "active";
     const pathname = usePathname();
-    const tl = useTranslations("Licenses");
 
     return (
-        <Tabs
-            defaultValue="active"
-            value={tab}
-            onValueChange={(value) => router.push(`${pathname}?tab=${value}`)}
-            className="flex flex-col w-full"
-        >
-            <TabsList className="max-w-fit">
-                <TabsTrigger value="passive">{tl("passive")}</TabsTrigger>
-                <TabsTrigger value="active">{tl("active")}</TabsTrigger>
-                <TabsTrigger value="completed">{tl("completed")}</TabsTrigger>
-                <TabsTrigger value="expired">{tl("expired")}</TabsTrigger>
-            </TabsList>
-            <TabsContent value="passive">
-                <PassiveTab />
-            </TabsContent>
-            <TabsContent value="active">
-                <ActiveTab />
-            </TabsContent>
-            <TabsContent value="completed">
-                <CompletedTab />
-            </TabsContent>
-            <TabsContent value="expired">
-                <ExpiredTab />
-            </TabsContent>
-        </Tabs>
+        <>
+            <div className="col-span-full text-sm text-muted-foreground">
+                <sup>*</sup>
+                {t("licenseCardWarning")}
+            </div>
+
+            <Tabs
+                defaultValue="active"
+                value={tab}
+                onValueChange={(value) =>
+                    router.push(`${pathname}?tab=${value}`)
+                }
+                className="flex flex-col w-full"
+            >
+                <TabsList className="max-w-fit">
+                    <TabsTrigger value="passive">{tl("passive")}</TabsTrigger>
+                    <TabsTrigger value="active">{tl("active")}</TabsTrigger>
+                    <TabsTrigger value="completed">
+                        {tl("completed")}
+                    </TabsTrigger>
+                    <TabsTrigger value="expired">{tl("expired")}</TabsTrigger>
+                </TabsList>
+                <TabsContent value="passive">
+                    <PassiveTab />
+                </TabsContent>
+                <TabsContent value="active">
+                    <ActiveTab />
+                </TabsContent>
+                <TabsContent value="completed">
+                    <CompletedTab />
+                </TabsContent>
+                <TabsContent value="expired">
+                    <ExpiredTab />
+                </TabsContent>
+            </Tabs>
+        </>
     );
 }
