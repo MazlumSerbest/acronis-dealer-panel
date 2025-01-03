85108fc1c5f2ef46cc475b6a6684899624bd6432
Mazlum Serbest <mazlumserbest93@gmail.com>
2025-01-03 15:43:46 +0300

    redirect added to auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -69,5 +69,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                 return true; //if the email exists in the User collection, email them a magic login link
             }
         },
+        async redirect({ url, baseUrl }) {
+            // Allows relative callback URLs
+            if (url.startsWith("/")) return `${baseUrl}${url}`;
+
+            // Allows callback URLs on the same origin
+            if (new URL(url).origin === baseUrl) return url;
+
+            return baseUrl;
+        },
     },
 });
