f9ca477e23472a2bf952f756307a86b0dbe922ca
Mazlum Serbest <mazlumserbest93@gmail.com>
2025-01-23 22:31:08 +0300

    Zebra browser print deleted

--- src/utils/zpl.ts
+++ src/utils/zpl.ts
@@ -1,31 +1,3 @@
-import ZebraBrowserPrintWrapper from "zebra-browser-print-wrapper";
-
-export async function printZPL(zpl: string) {
-    try {
-        // Create a new instance of the object
-        const browserPrint = new ZebraBrowserPrintWrapper();
-
-        // Select default printer
-        const defaultPrinter = await browserPrint.getDefaultPrinter();
-
-        // Set the printer
-        browserPrint.setPrinter(defaultPrinter);
-
-        // Check printer status
-        const printerStatus = await browserPrint.checkPrinterStatus();
-
-        // Check if the printer is ready
-        if (printerStatus.isReadyToPrint) {
-            browserPrint.print(zpl);
-            return { ok: true };
-        } else {
-            return { message: printerStatus.errors, ok: false };
-        }
-    } catch (error) {
-        return { message: error, ok: false };
-    }
-}
-
 export async function createPDF(zpl: string) {
     await fetch(`${window.location.protocol}//api.labelary.com/v1/printers/8dpmm/labels/2.36x0.59`, {
         method: "POST",
