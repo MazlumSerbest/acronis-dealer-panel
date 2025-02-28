480108d4e4ab24cb5985962b3a73300de227a0d4
Mazlum Serbest <mazlumserbest93@gmail.com>
2025-02-28 14:33:31 +0300

    Paraşüt API getToken method added

--- src/lib/getToken.ts
+++ src/lib/getToken.ts
@@ -1,12 +1,12 @@
 import "server-only";
 
 export async function getAcronisToken() {
-    const encodedClientCreds = btoa(
+    const encodedClientCredentials = btoa(
         `${process.env.ACRONIS_CLIENT_ID}:${process.env.ACRONIS_CLIENT_SECRET}`,
     );
     const headers = {
         "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: `Basic ${encodedClientCreds}`,
+        Authorization: `Basic ${encodedClientCredentials}`,
     };
     const res = await fetch(`${process.env.ACRONIS_API_V2_URL}/idp/token`, {
         method: "POST",
@@ -14,7 +14,7 @@ export async function getAcronisToken() {
         body: new URLSearchParams({
             grant_type: "client_credentials",
         }),
-        next: { revalidate: 5400 },
+        next: { revalidate: 7200 },
         // next: { revalidate: 0 },
     });
 
@@ -22,4 +22,20 @@ export async function getAcronisToken() {
     return await auth.access_token;
 }
 
-export async function getParasutToken() {}
+export async function getParasutToken() {
+    const res = await fetch(`${process.env.PARASUT_BASE_URL}/oauth/token`, {
+        method: "POST",
+        body: new URLSearchParams({
+            grant_type: "password",
+            client_id: process.env.PARASUT_CLIENT_ID ?? "",
+            client_secret: process.env.PARASUT_CLIENT_SECRET ?? "",
+            username: process.env.PARASUT_USERNAME ?? "",
+            password: process.env.PARASUT_PASSWORD ?? "",
+            redirect_uri: process.env.PARASUT_CALLBACK_URL ?? "",
+        }),
+        next: { revalidate: 7200 },
+    });
+
+    const auth = await res.json();
+    return await auth.access_token;
+}
