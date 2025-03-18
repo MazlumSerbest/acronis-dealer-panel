f298adcf7be9d13b144207e42cc2a5728e9a326d
Mazlum Serbest <mazlumserbest93@gmail.com>
2025-03-18 23:48:36 +0300

    draft status name changed to potential

--- src/app/[locale]/panel/@admin/potentialPartners/page.tsx
+++ src/app/[locale]/panel/@admin/potentialPartners/page.tsx
@@ -3,11 +3,10 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useTranslations } from "next-intl";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import DraftTab from "./(tabs)/Draft";
+import PotentialTab from "./(tabs)/Potential";
 import ContactedTab from "./(tabs)/Contacted";
 import WonTab from "./(tabs)/Won";
 import LostTab from "./(tabs)/Lost";
-import PageHeader from "@/components/PageHeader";
 
 export default function PotentialPartnersPage() {
     const t = useTranslations("General");
@@ -15,7 +14,7 @@ export default function PotentialPartnersPage() {
     const router = useRouter();
     const searchParams = useSearchParams();
     const pathname = usePathname();
-    const tab = searchParams.get("tab") || "draft";
+    const tab = searchParams.get("tab") || "potential";
 
     return (
         <Tabs
@@ -25,14 +24,14 @@ export default function PotentialPartnersPage() {
             className="flex flex-col w-full"
         >
             <TabsList className="mx-auto  md:*:w-[200px] *:w-full mb-2">
-                <TabsTrigger value="draft">{t("draft")}</TabsTrigger>
+                <TabsTrigger value="potential">{t("potential")}</TabsTrigger>
                 <TabsTrigger value="contacted">{t("contacted")}</TabsTrigger>
                 <TabsTrigger value="won">{t("won")}</TabsTrigger>
                 <TabsTrigger value="lost">{t("lost")}</TabsTrigger>
             </TabsList>
 
-            <TabsContent value="draft">
-                <DraftTab />
+            <TabsContent value="potential">
+                <PotentialTab />
             </TabsContent>
             <TabsContent value="contacted">
                 <ContactedTab />
